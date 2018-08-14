f3f6018a821ea252bdc6dd7500e690da58d5ec01
administrator <administrator@iZ25i0hktciZ>
2018-08-14 10:35:32 +0800

    配置tomcat部署

--- src/main/java/com/fish/FishBootApplication.java
+++ src/main/java/com/fish/FishBootApplication.java
@@ -3,11 +3,25 @@ package com.fish;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.fish.dao")
-public class FishBootApplication {
+public class FishBootApplication  extends SpringBootServletInitializer{
 
+	/**
+	 * 重写configure方法
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FishBootApplication.class);
+	}
+	
+	/**
+	 * 程序主函数
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FishBootApplication.class, args);
 	}
